0a605dcf8d2e69a32566ddcca282625b73c5237e
Santiago Daniel <98777824+dannfront@users.noreply.github.com>
2024-02-03 22:48:37 -0600

    Update functionsDestinations.js

--- src/js/functionsDestinations.js
+++ src/js/functionsDestinations.js
@@ -2,7 +2,7 @@ import {about,title,distance,travel,imgDestination} from './destination.js'
 
 
 export function renderData(data){
-    imgDestination.src=`../../starter-code/${data.images.png}`
+    imgDestination.src=`/space-tourism/starter-code/${data.images.png}`
     about.textContent=data.description
     title.textContent=data.name
     travel.textContent=data.travel
